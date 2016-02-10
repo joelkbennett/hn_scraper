55906f25e671ce07dd77575e666d3a8470d6c1b4
joelkbennett <joelkbennett@gmail.com>
2016-02-10 15:16:21 +0000

    Add specs for needed classes; Add colorize dep

---
+++ spec/post_spec.rb
@@ -0,0 +1,9 @@
+require_relative "spec_helper"
+
+describe Post do
+
+  # Test for title, url, points, item_id
+  # Test for methods #comments (return comments) and 
+  #   #add_comment (take comment obj; adds to comment list)
+
+end
